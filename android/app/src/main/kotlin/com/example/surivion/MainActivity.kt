ba0e59c37c1985e834ec8425433f3940b4cb1ece
Jomar029 <jplibramonte999@gmail.com>
2026-02-27 17:29:40 +0800

    1st com

---
+++ android/app/src/main/kotlin/com/example/surivion/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.surivion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SuriVionApp());
+}
+
+class SuriVionApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SuriVion',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("SuriVion Waste Scanner"),
+        centerTitle: true,
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            Icon(
+              Icons.recycling,
+              size: 120,
+              color: Colors.green,
+            ),
+
+            SizedBox(height: 20),
+
+            Text(
+              "SuriVion",
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            SizedBox(height: 10),
+
+            Text(
+              "Offline Waste Scanner",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey,
+              ),
+            ),
+
+            SizedBox(height: 40),
+
+            ElevatedButton(
+              onPressed: () {
+
+              },
+              child: Text(
+                "Scan Waste",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
